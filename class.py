5b31a815c53df8905a96ac3a16a3180816529549
Ganesh-337 <84136877+Ganesh-337@users.noreply.github.com>
2022-10-20 18:15:34 +0530

    Update class.py

--- class.py
+++ class.py
@@ -2,8 +2,8 @@ class numbers:
     num1 = 0.0
     num2 = 0.0
     def Set(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+	self.num1 = num1
+	self.num2 = num2
         
 
 def Main():
@@ -12,4 +12,4 @@ def Main():
 	print("X= " + str(num.num1) + ", Y= " + str(num.num2))
 
 if __name__=='__main__':
-		Main()
+	Main()

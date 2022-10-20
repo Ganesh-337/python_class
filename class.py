f89658ff7f04eb8056a3f5a687351707f8776916
Ganesh-337 <84136877+Ganesh-337@users.noreply.github.com>
2022-10-20 18:14:12 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,15 @@
+class numbers:
+    num1 = 0.0
+    num2 = 0.0
+    def Set(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+
+def Main():
+	num = numbers()
+	num.Set(7, 8)	
+	print("X= " + str(num.num1) + ", Y= " + str(num.num2))
+
+if __name__=='__main__':
+		Main()
